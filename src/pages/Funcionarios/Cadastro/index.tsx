7a06d1167f895790adba57fab333fe9fcbe8a386
Gabriel <gabriel.devloper@gmail.com>
2021-04-08 01:20:10 -0300

    feat: tela de carregamento

--- src/pages/Funcionarios/Cadastro/index.tsx
+++ src/pages/Funcionarios/Cadastro/index.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { Button, Text, StyleSheet, View, Image, Alert } from "react-native";
+import {
+  Button,
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  Alert,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 import { TextInput, TouchableHighlight } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TextInputMask } from "react-native-masked-text";
@@ -7,10 +16,10 @@ import { useNavigation } from "@react-navigation/native";
 import { Dashboard } from "../../../components/Dashboard";
 import { Toast } from "native-base";
 import { api } from "../../../services/api";
-import AppLoading from "expo-app-loading";
 
 export function CadastroFuncionario() {
   const navigation = useNavigation();
+  const [loading, setLoading] = useState(false);
 
   const [nome, setNome] = useState("");
   const [data_nascimento, setDataNascimento] = useState("");
@@ -28,74 +37,103 @@ export function CadastroFuncionario() {
     //const response = await api.get("/api/usuarios");
 
     try {
-      const response = await api.post("/api/usuarios", data);
+      setLoading(true);
 
-      if (!response.data) {
-        return <AppLoading />;
-      }
+      await api.post("/api/usuarios", data);
+
+      setLoading(false);
 
       Toast.show({
         text: "Funcionario cadastrado com sucesso",
         buttonText: "ok",
+        position: "top",
+        duration: 3000,
+        style: {
+          marginTop: 59,
+          backgroundColor: "#2abb06",
+        },
       });
 
-      console.log(response);
+      setNome("");
+      setCpf("");
+      setDataNascimento("");
+      setSetor("");
     } catch {
+      setLoading(false);
       Toast.show({
         text: "Erro ao cadastrar um funcionario",
         buttonText: "ok",
+        position: "top",
+        duration: 3000,
+        style: {
+          marginTop: 59,
+          backgroundColor: "#eb2b2b",
+        },
       });
     }
   }
 
   return (
-    <>
-      <Dashboard>
-        <Text style={styles.title}>Cadastro de funcionário</Text>
-        <SafeAreaView>
-          <TextInput
-            style={styles.input}
-            onChangeText={setNome}
-            value={nome}
-            placeholder="Nome"
-          />
+    <Dashboard>
+      {loading ? (
+        <ActivityIndicator size={200} color="#2196F3" />
+      ) : (
+        <>
+          <Text style={styles.title}>Cadastro de funcionário</Text>
+          <SafeAreaView>
+            <TextInput
+              style={styles.input}
+              onChangeText={setNome}
+              value={nome}
+              placeholder="Nome"
+            />
 
-          <TextInputMask
-            type={"custom"}
-            options={{
-              mask: "99/99/9999",
-            }}
-            style={styles.input}
-            onChangeText={setDataNascimento}
-            value={data_nascimento}
-            placeholder="Data de nascimento"
-          />
+            <TextInputMask
+              type={"custom"}
+              options={{
+                mask: "99/99/9999",
+              }}
+              style={styles.input}
+              onChangeText={setDataNascimento}
+              value={data_nascimento}
+              placeholder="Data de nascimento"
+            />
 
-          <TextInputMask
-            type={"cpf"}
-            style={styles.input}
-            value={cpf}
-            onChangeText={setCpf}
-            placeholder="CPF"
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={setSetor}
-            value={setor}
-            placeholder="Setor"
-          />
-          <View style={styles.buttonSalvar}>
-            <Button title="Salvar" onPress={handleSalvar} />
-          </View>
-        </SafeAreaView>
-      </Dashboard>
-    </>
+            <TextInputMask
+              type={"cpf"}
+              style={styles.input}
+              value={cpf}
+              onChangeText={setCpf}
+              placeholder="CPF"
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={setSetor}
+              value={setor}
+              placeholder="Setor"
+            />
+
+            <TouchableOpacity
+              onPress={handleSalvar}
+              style={styles.buttonSalvar}
+            >
+              <Text style={styles.titleButtonText}>Cadastrar funcionário</Text>
+            </TouchableOpacity>
+          </SafeAreaView>
+        </>
+      )}
+    </Dashboard>
   );
 }
 
 const styles = StyleSheet.create({
   buttonSalvar: {
+    width: 300,
+    padding: 2,
     marginTop: 50,
+    backgroundColor: "#2196F3",
+    borderRadius: 10,
+    alignItems: "center",
   },
 
   input: {
@@ -111,5 +149,12 @@ const styles = StyleSheet.create({
   title: {
     margin: 20,
     fontSize: 20,
+    color: "black",
+  },
+
+  titleButtonText: {
+    margin: 20,
+    fontSize: 20,
+    color: "white",
   },
 });
